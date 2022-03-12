31b9019a1402c967bc389935c4b51f14904869cf
Marko Otlokan <otlokanmarko@gmail.com>
2022-03-12 01:15:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt upgrade
 sudo apt remove gnome-shell-extension-ubuntu-dock
-sudo apt install fish python3 python3-pip python3-venv htop screenfetch net-tools mpv unp gparted docker docker-compose openjdk-11-jre-headless openjdk-8-jre-headless openssh-server libreoffice nvidia-cuda-toolkit intel-gpu-tools virtualbox openshot wireshark tshark simplescreenrecorder stress lm-sensors
+sudo apt install fish python3 python3-pip python3-venv htop screenfetch net-tools mpv unp gparted docker docker-compose openjdk-11-jre-headless openjdk-8-jre-headless openssh-server libreoffice nvidia-cuda-toolkit intel-gpu-tools virtualbox openshot wireshark tshark simplescreenrecorder stress lm-sensors qbittorrent
 sudo snap install telegram-desktop
 sudo apt autoremove
 chsh -s '/usr/bin/fish'
