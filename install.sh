7760dc68f2bc22ba709e4496c9293f386e5c26d3
Marko Otlokan <otlokanmarko@gmail.com>
2023-11-13 09:35:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 sudo apt update
 sudo apt upgrade
 sudo apt remove gnome-shell-extension-ubuntu-dock
-sudo apt install google-chrome-stable fish python3 python3-pip python3-venv htop screenfetch net-tools mpv unp gparted docker docker-compose openjdk-11-jre-headless openjdk-8-jre-headless openssh-server libreoffice nvidia-cuda-toolkit intel-gpu-tools virtualbox wireshark tshark simplescreenrecorder stress lm-sensors qbittorrent chrome-gnome-shell glmark2 cmatrix nvtop p7zip-full nvidia-cuda-toolkit
+sudo apt install google-chrome-stable fish python3 python3-pip htop screenfetch net-tools mpv unp gparted openjdk-11-jre-headless openssh-server libreoffice nvidia-cuda-toolkit intel-gpu-tools virtualbox wireshark tshark simplescreenrecorder stress lm-sensors qbittorrent chrome-gnome-shell glmark2 cmatrix nvtop p7zip-full nvidia-cuda-toolkit
 sudo snap refresh
-sudo snap install telegram-desktop skype
+sudo snap install telegram-desktop
 sudo snap refresh
-pip3 install keras tensorflow torch jupyter pandas regex scipy seaborn Flask pyspark peewee sklearn opencv-python peewee python-dotenv websocket torchvision torchsummary segmentation_models_pytorch numpy
+pip3 install keras tensorflow torch jupyter pandas regex scipy seaborn Flask pyspark peewee sklearn opencv-python peewee python-dotenv websocket segmentation_models_pytorch numpy
 sudo apt autoremove
 chsh -s '/usr/bin/fish'
