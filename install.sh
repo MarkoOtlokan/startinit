ace9cb6a3a9dd6385e3095dd825ef028bbd4897b
Marko Otlokan <otlokanmarko@gmail.com>
2025-08-30 20:27:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 sudo apt update
 sudo apt upgrade
 sudo apt remove gnome-shell-extension-ubuntu-dock
-sudo apt install google-chrome-stable fish python3 python3-pip htop screenfetch net-tools mpv unp gparted openjdk-11-jre-headless openssh-server libreoffice nvidia-cuda-toolkit intel-gpu-tools virtualbox wireshark tshark simplescreenrecorder stress lm-sensors qbittorrent chrome-gnome-shell glmark2 cmatrix nvtop p7zip-full nvidia-cuda-toolkit
+sudo apt install google-chrome-stable fish python3 python3-pip htop screenfetch net-tools mpv unp gparted openssh-server libreoffice simplescreenrecorder  qbittorrent chrome-gnome-shell glmark2 nvtop p7zip-full
 sudo snap refresh
 sudo snap install telegram-desktop
 sudo snap refresh
-pip3 install keras tensorflow torch jupyter pandas regex scipy seaborn Flask pyspark peewee sklearn opencv-python peewee python-dotenv websocket segmentation_models_pytorch numpy
+pip3 install keras tensorflow torch jupyter pandas scipy seaborn pyspark
 sudo apt autoremove
 chsh -s '/usr/bin/fish'
