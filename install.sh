7c9b06ea9dfa77cf4c4fe352e9b204f086cfafff
Marko Otlokan <otlokanmarko@gmail.com>
2025-08-30 23:44:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt remove gnome-shell-extension-ubuntu-dock
 sudo apt install google-chrome-stable fish python3 python3-pip htop screenfetch net-tools mpv unp gparted openssh-server libreoffice simplescreenrecorder  qbittorrent chrome-gnome-shell glmark2 nvtop p7zip-full
 sudo snap refresh
 sudo snap install telegram-desktop
+sudo snap install slack
 sudo snap refresh
 pip3 install keras tensorflow torch jupyter pandas scipy seaborn pyspark
 sudo apt autoremove
